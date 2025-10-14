8665cfeb285cb41fdee5f3066f69695bad440024
Siddharth Koli <siddharthkoli2401@gmail.com>
2025-10-14 22:39:58 +0530

    add secrets example header

---
+++ include/secrets.example.h
@@ -0,0 +1,12 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi credentials
+#define ENV_WIFI_SSID ""
+#define ENV_WIFI_PASS ""
+
+// Supabase 
+#define ENV_EDGE_FUNCTION_URL ""
+#define ENV_JWT ""
+
+#endif
