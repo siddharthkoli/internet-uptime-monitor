1e14cfaee1786bf90f217b2d03c21af0ef95ddfc
Siddharth Koli <siddharthkoli2401@gmail.com>
2025-10-21 00:36:08 +0530

    move default delay to main file

--- include/utils.h
+++ include/utils.h
@@ -1,11 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-// Blink defaults
-static const int DEFAULT_BLINK_DELAY_MS = 250;
-
 // Blink helpers
 void blinkLED(int delayMs);
-void blinkLED(int times, int delayMs = DEFAULT_BLINK_DELAY_MS);
+void blinkLED(int times, int delayMs);
 
 #endif
