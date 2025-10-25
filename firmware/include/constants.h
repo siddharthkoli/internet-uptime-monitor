2b2c9096bc7b866d2ab6c6d015a59b081ac86b8b
Siddharth Koli <siddharthkoli2401@gmail.com>
2025-10-25 14:57:38 +0530

    create global constants

---
+++ firmware/include/constants.h
@@ -0,0 +1,31 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include "secrets.h"
+
+// WIFI Credentials
+static const char* WIFI_SSID = ENV_WIFI_SSID;
+static const char* WIFI_PASS = ENV_WIFI_PASS;
+
+// Edge function that stores uptime data
+static const char* EDGE_FUNCTION_URL = ENV_EDGE_FUNCTION_URL;
+static const char* JWT = ENV_JWT;
+
+// Log Ingestion Endpoint
+static const char* LOG_INGESTION_URL = ENV_LOG_INGESTION_URL;
+
+// Device ID
+static const char* DEVICE_ID = "ESP32-01";
+
+// LED Blink patterns
+static const int LED_BLINK_SUCCESS = 1;
+static const int LED_BLINK_FAILURE = 2;
+static const int DEFAULT_BLINK_DELAY_MS = 250;
+
+// Device storage
+static const int STORAGE_CLEAR_BEFORE_BEGIN = false;
+
+// Network
+static const long SEND_INTERVAL = 60'000;  // 1 min
+
+#endif
